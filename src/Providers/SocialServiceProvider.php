ec1b11fe178b87ce64cd8516482008052b3a7575
miladimos <message.tome1331@gmail.com>
2020-10-14 18:06:09 +0330

    Add Social Service Provider

---
+++ src/Providers/SocialServiceProvider.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Miladimos\Social\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+
+class SocialServiceProvider extends ServiceProvider
+{
+
+    public function register()
+    {
+        $this->mergeConfigFrom(__DIR__ . "/../../config/config.php", 'social');
+
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+
+        if ($this->app->runningInConsole()) {
+
+            $this->registerPublishes();
+            
+        }
+    }
+
+    private function registerPublishes()
+    {
+        $this->publishes([
+            __DIR__ . '/../../config/config.php' => config_path('social.php')
+        ], 'social-config');
+
+    }
+}
