003a39d0ef5be40cc99354710db25122e265d672
miladimos <milad.jafari6210@gmail.com>
2020-10-16 20:00:24 +0330

    Add likes table migration

--- src/Providers/SocialServiceProvider.php
+++ src/Providers/SocialServiceProvider.php
@@ -44,5 +44,13 @@ class SocialServiceProvider extends ServiceProvider
             __DIR__ . '/../../config/config.php' => config_path('social.php')
         ], 'social-config');
 
+
+        if (! class_exists('CreatePostsTable')) {
+            $this->publishes([
+                __DIR__ . '/../database/migrations/create_posts_table.php.stub' => database_path('migrations/' . date('Y_m_d_His', time()) . '_create_posts_table.php'),
+                // you can add any number of migrations here
+            ], 'migrations');
+        }
+
     }
 }
