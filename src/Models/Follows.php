d12f8eeaeddee9a09eca39ae11272f2841dfb04b
miladimos <milad.jafari6210@gmail.com>
2021-01-08 18:17:29 +0330

    add traits

---
+++ src/Models/Follows.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Miladimos\Social\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Like extends Model
+{
+//    protected $table = config('social.likes.table', 'likes');
+    protected $table = 'likes';
+
+    protected $fillable = ['user_id', 'likeable_id', 'likeable_type'];
+
+    public $timestamps = true;
+
+    public function likeable()
+    {
+        return $this->morphTo();
+    }
+
+}
