67e3d3abdb31b7d253fd6d8316733d07f34d74db
miladimos <milad.jafari6210@gmail.com>
2022-02-12 21:13:26 +0330

    FUNDING github

--- src/Providers/SocialServiceProvider.php
+++ src/Providers/SocialServiceProvider.php
@@ -54,10 +54,10 @@ class SocialServiceProvider extends ServiceProvider
 
     private function registerMigrations()
     {
-        if (!class_exists('CreateTagsTable')) {
-            $this->publishes([
-                __DIR__ . '/../../database/migrations/create_tags_table.php' => database_path('migrations/' . date('Y_m_d_His', time()) . '_create_tags_tables.php'),
-            ], 'migrations');
-        }
+        // if (!class_exists('CreateTagsTable')) {
+        $this->publishes([
+            __DIR__ . '/../../database/migrations/create_tags_table.php' => database_path('migrations/' . date('Y_m_d_His', time()) . '_create_tags_tables.php'),
+        ], 'migrations');
+        // }
     }
 }
