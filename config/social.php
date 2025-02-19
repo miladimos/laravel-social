77fa0490a3a931b230f769d47baacd8ea80f6a22
miladimos <milad.jafari6210@gmail.com>
2025-02-19 19:18:50 +0330

    improve

--- config/social.php
+++ config/social.php
@@ -3,14 +3,6 @@
 // config for miladimos/laravel-social
 return [
 
-    'tags' => [
-        'model' => Miladimos\Social\Models\Tag::class,
-        'table' => 'social_tags',
-        'morphs' => 'taggables',
-        'morphs_table' => 'social_taggables',
-    ],
-
-    // follow/unfollow - subscription/unsubscription or ...
     'follows' => [
         'model' => Miladimos\Social\Models\Follows::class,
         'table' => 'social_follows',
@@ -20,46 +12,9 @@ return [
     ],
 
 
-    'categories' => [
-        'model' => Miladimos\Social\Models\Category::class,
-
-        'morphs' => 'categoriable',
-    ],
-
     'likes' => [
         'model' => Miladimos\Social\Models\Like::class,
 
         'liker_foreign_key' => 'liker_id',
-    ],
-
-    'bookmarks' => [
-        'model' => Miladimos\Social\Models\Bookmark::class,
-
-        'morphs' => 'bookmarkable',
-
-        'bookmarker_foreign_key' => 'user_id',
-
-        'bookmark_group' => [
-            'model' => Miladimos\Social\Models\BookmarkGroup::class,
-        ],
-    ],
-
-
-    'comments' => [
-        'model' => Miladimos\Social\Models\Comment::class,
-
-        'commentors' => [],
-
-        'morphs' => 'commentable',
-
-        'commentor_foreign_key' => 'commentor_id',
-
-        'default_commentator' => config('auth.providers.users.model'),
-
-        'middleware'   => ['web'],
-
-        'need_approve' => false,
-
-        'has_rate' => false,
     ],
 ];
--- database/migrations/create_socials_table.php
+++ database/migrations/create_socials_table.php
@@ -6,32 +6,8 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
+    public function up(): void
     {
-        Schema::create(config('social.tags.table'), function (Blueprint $table) {
-            $table->id();
-            $table->string('name')->unique()->index();
-            $table->string('slug')->unique();
-            $table->softDeletes();
-            $table->timestamps();
-        });
-
-        Schema::create(config('social.tags.morphs_table'), function (Blueprint $table) {
-            $table->foreignId('tag_id');
-            $table->morphs(config('social.tags.morphs'));
-
-            $table->foreign('tag_id')
-                ->references('id')
-                ->on(config('social.tags.table'))
-                ->onUpdate('cascade')
-                ->onDelete('cascade');
-        });
-
         Schema::create(config('social.follows.table'), function (Blueprint $table) {
             $table->id();
             $table->morphs('followable'); // follower
@@ -41,57 +17,19 @@ return new class extends Migration
             $table->timestamps();
         });
 
-        //        Schema::create(config('social.bookmarks.table'), function (Blueprint $table) {
-        //            $table->id();
-        //            $table->unsignedBigInteger(config('social.bookmarks.bookmarker_foreign_key'))->index()->comment('user_id');
-        //            $table->morphs(config('social.bookmarks.morphs'));
-        //        });
-        //
-        //        Schema::create(config('social.comments.table'), function (Blueprint $table) {
-        //            $table->id();
-        //            $table->foreignId('parent_id')->default(0);
-        //            $table->morphs('commentorable');
-        //            $table->morphs('commentable');
-        //            $table->text('comment');
-        //            $table->string('guest_name')->nullable();
-        //            $table->string('guest_email')->nullable();
-        //            $table->boolean('is_approved')->default(false);
-        //            $table->timestamp('approved_at')->nullable();
-        //            $table->timestamps();
-        //
-        //            $table->index(["commentable_type", "commentable_id"]);
-        //
-        //            $table->foreign('parent_id')
-        //                ->references('id')
-        //                ->on(config('social.comments.table'))->onDelete('cascade')->onUpdate('cascade');
-        //        });
-        //
-        //        Schema::create(config('social.likes.table'), function (Blueprint $table) {
-        //            $table->id();
-        //            $table->foreignId(config('social.likes.liker_foreign_key'))->index()->comment('user_id');
-        //            $table->morphs(config('social.likes.morphs'));
-        //            $table->timestamps();
-        //
-        //            $table->unique(['likeable_id', 'likeable_type'], 'likes');
-        //        });
+        Schema::create(config('social.likes.table'), function (Blueprint $table) {
+            $table->id();
+            $table->foreignId(config('social.likes.liker_foreign_key'))->index()->comment('user_id');
+            $table->morphs(config('social.likes.morphs'));
+            $table->timestamps();
+
+            $table->unique(['likeable_id', 'likeable_type'], 'likes');
+        });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
+    public function down(): void
     {
-        Schema::dropIfExists(config('social.tags.table'));
-        Schema::dropIfExists(config('social.tags.morphs_table'));
         Schema::dropIfExists(config('social.follows.table'));
-
-        //        Schema::dropIfExists(config('social.bookmarks_group.table'));
-        //        Schema::dropIfExists(config('social.bookmarks.table'));
-        //        Schema::dropIfExists(config('social.categories.table'));
-        //        Schema::dropIfExists(config('social.categories.morphs'));
-        //        Schema::dropIfExists(config('social.comments.table'));
-        //        Schema::dropIfExists(config('social.likes.table'));
+        Schema::dropIfExists(config('social.likes.table'));
     }
 };
