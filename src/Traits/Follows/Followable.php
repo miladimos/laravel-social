27d99644701ebb2b7f73d3c6121fd73f1e009362
miladimos <milad.jafari6210@gmail.com>
2025-07-11 20:28:27 +0330

    improve

--- src/Traits/Follows/Followable.php
+++ src/Traits/Follows/Followable.php
@@ -3,26 +3,14 @@
 namespace Miladimos\Social\Traits\Follows;
 
 use DateTime;
-use function collect;
-use function abort_if;
 use function in_array;
-use function is_array;
 use function class_uses;
 use InvalidArgumentException;
-use function iterator_to_array;
-use Illuminate\Support\Enumerable;
-use Illuminate\Support\LazyCollection;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Pagination\CursorPaginator;
 use Illuminate\Database\Eloquent\Collection;
-use Illuminate\Contracts\Pagination\Paginator;
-use Illuminate\Pagination\LengthAwarePaginator;
-use Miladimos\Social\Contracts\CanFollowContract;
-use Miladimos\Social\Contracts\FollowableContract;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\MorphMany;
-
 
 trait Followable
 {
@@ -46,64 +34,12 @@ trait Followable
 
     public function followers(): MorphMany
     {
-        return $this->morphMany(config('social.follows.model'), 'followable')->withPivot('accepted_at')->withTimestamps();
+        return $this->morphMany($this->followsModel(), 'followable');
     }
 
     public function followings(): MorphMany
     {
-        return $this->morphMany(config('social.follows.model'), 'followingable');
-    }
-
-    public function rejectFollowRequestFrom(Model $follower): void
-    {
-        if (! in_array(Follower::class, \class_uses($follower))) {
-            throw new \InvalidArgumentException('The model must use the Follower trait.');
-        }
-
-        // $this->followers()->detach($user);
-
-        $this->followables()->followedBy($follower)->get()->each->delete();
-    }
-
-    public function acceptFollowRequestFrom(Model $follower): void
-    {
-        if (! in_array(Follower::class, \class_uses($follower))) {
-            throw new \InvalidArgumentException('The model must use the Follower trait.');
-        }
-        // $this->followers()->updateExistingPivot($user, ['accepted_at' => now()]);
-
-        $this->followables()->followedBy($follower)->get()->each->update(['accepted_at' => \now()]);
-    }
-
-    public function isFollowedBy(Model $follower): bool
-    {
-        if (! in_array(Follower::class, \class_uses($follower))) {
-            throw new \InvalidArgumentException('The model must use the Follower trait.');
-        }
-
-        if ($this->relationLoaded('followables')) {
-            return $this->followables->whereNotNull('accepted_at')->contains($follower);
-        }
-
-        return $this->followables()->accepted()->followedBy($follower)->exists();
-
-
-
-        if ($this->relationLoaded('followers')) {
-            return $this->followers()
-                ->wherePivot('accepted_at', '!=', null)
-                ->contains($user);
-        }
-    }
-
-    public function approvedFollowers()
-    {
-        return $this->followers()->whereNotNull('accepted_at');
-    }
-
-    public function notApprovedFollowers()
-    {
-        return $this->followers()->whereNull('accepted_at');
+        return $this->morphMany($this->followsModel(), 'followingable');
     }
 
     public function follow(Model $followable): array
@@ -116,7 +52,6 @@ trait Followable
             throw new InvalidArgumentException('The followable model must use the Followable trait.');
         }
 
-        /** @var \Illuminate\Database\Eloquent\Model|\Overtrue\LaravelFollow\Traits\Followable $followable */
         $isPending = $followable->needsToApproveFollowRequests() ?: false;
 
         $this->followings()->updateOrCreate([
@@ -128,11 +63,6 @@ trait Followable
 
         return ['pending' => $isPending];
 
-
-        // $following = $this->findFollowing($entity);
-
-        // $isPending = $entity->needsToApproveFollowRequests() ?: false;
-
         // if (!$this->isFollowing($entity) && $this->id != $entity->id) {
         //     return $this->followings()->attach($entity);
         // }
@@ -140,36 +70,66 @@ trait Followable
         // $this->followings()->attach($user, [
         //     'accepted_at' => $isPending ? null : now()
         // ]);
-
-        // return ['pending' => $isPending];
     }
 
-    public function unfollow(Model $followable): void
+    public function unfollow(Model $followable)
     {
+        if ($followable->is($this)) {
+            throw new InvalidArgumentException('Cannot unfollow yourself.');
+        }
+
         if (! in_array(Followable::class, class_uses($followable))) {
             throw new InvalidArgumentException('The followable model must use the Followable trait.');
         }
 
-        $this->followings()->of($followable)->get()->each->delete();
+        // $this->followings()->of($followable)->get()->each->delete();
 
-
-
-        //  $this->followings()->detach($user);
-
-        // $relation = $object->subscriptions()
-        //     ->where('subscribable_id', $object->getKey())
-        //     ->where('subscribable_type', $object->getMorphClass())
-        //     ->where(config('social.subscribtions.user_foreign_key'), $this->getKey())
-        //     ->first();
-
-        // if ($relation) {
-        //     $relation->delete();
-        // }
+        return $this->followings()->detach($followable);
     }
 
     public function toggleFollow(Model $followable): void
     {
         $this->isFollowing($followable) ? $this->unfollow($followable) : $this->follow($followable);
+    }
+
+    public function rejectFollowRequestFrom(Model $follower): void
+    {
+        if (! in_array(Followable::class, \class_uses($follower))) {
+            throw new \InvalidArgumentException('The model must use the Follower trait.');
+        }
+
+        // $this->followers()->detach($user);
+
+        $this->followables()->followedBy($follower)->get()->each->delete();
+    }
+
+    public function acceptFollowRequestFrom(Model $follower): void
+    {
+        if (! in_array(Followable::class, \class_uses($follower))) {
+            throw new \InvalidArgumentException('The model must use the Follower trait.');
+        }
+        // $this->followers()->updateExistingPivot($user, ['accepted_at' => now()]);
+
+        $this->followables()->followedBy($follower)->get()->each->update(['accepted_at' => \now()]);
+    }
+
+    public function isFollowedBy(Model $follower): bool
+    {
+        if (! in_array(Followable::class, \class_uses($follower))) {
+            throw new \InvalidArgumentException('The model must use the Follower trait.');
+        }
+
+        if ($this->relationLoaded('followers')) {
+            return $this->followers->whereNotNull('accepted_at')->contains($follower);
+        }
+
+        return $this->followers()->accepted()->followedBy($follower)->exists();
+
+        // if ($this->relationLoaded('followers')) {
+        //     return $this->followers()
+        //         ->wherePivot('accepted_at', '!=', null)
+        //         ->contains($user);
+        // }
     }
 
     public function isFollowing(Model $followable): bool
@@ -199,6 +159,22 @@ trait Followable
         // //         ->where('subscribable_id', $object->getKey())
         // //         ->where('subscribable_type', $object->getMorphClass())
         // //         ->count() > 0;
+    }
+
+
+
+
+
+
+
+    public function approvedFollowers()
+    {
+        return $this->followers()->whereNotNull('accepted_at');
+    }
+
+    public function notApprovedFollowers()
+    {
+        return $this->followers()->whereNull('accepted_at');
     }
 
     public function hasRequestedToFollow(Model $followable): bool
@@ -254,16 +230,6 @@ trait Followable
     public function hasAnyFollowers(): bool
     {
         return (bool) $this->followers()->count();
-    }
-
-    public function findFollowing(FollowableContract $entity)
-    {
-        return $this->followings()->whereFollowableEntity($entity)->first();
-    }
-
-    public function findFollower(CanFollowContract $entity)
-    {
-        return $this->followers()->whereFollowingableEntity($entity)->first();
     }
 
     public function areFollowingEachOther($user): bool
